70fb5320c33f3620493ced76319869809017a729
Florian-EL <florian.eymerleprovost@gmail.com>
2026-05-30 09:56:48 +0200

    ajout multi tab (home stat et par type)

--- src/view/main_window.py
+++ src/view/main_window.py
@@ -8,9 +8,15 @@ from PyQt5.QtGui import QStandardItemModel, QStandardItem, QColor
 class CulturApp(QWidget) :
     def __init__(self) :
         super().__init__()
-        self.layout = QHBoxLayout()
         
-        self.right_menu_layout = QHBoxLayout() #TODO -> tabwidget ou filtre
+        self.cultur_menu = ["Films", "Serie_film", "Series", "Romans", "Manga", "Webtoon", "Wattpad", "Music"]
+        
+        self.init_ui()
+    
+    def init_ui(self) :
+        self.layout = QVBoxLayout()
+        self.right_menu_layout = QVBoxLayout()
+        self.window_layout = QHBoxLayout()
         self.stat_layout = QVBoxLayout()
         self.main_layout = QVBoxLayout()
         
@@ -22,9 +28,18 @@ class CulturApp(QWidget) :
         self.setLayout(self.layout)
         
     def set_menu(self) :
-        label = QLabel("Menu")
-        label.setStyleSheet("background-color: red;")
-        self.right_menu_layout.addWidget(label)
+        button_home = QPushButton("Home")
+        button_home.setStyleSheet("background-color: red;")
+        button_home.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Expanding)
+        #TODO button_home.clicked.connect(self.set_main)
+        self.right_menu_layout.addWidget(button_home)
+        
+        for menu in self.cultur_menu :
+            button = QPushButton(menu)
+            button.setStyleSheet("background-color: red;")
+            button.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Expanding)
+            #TODO button.clicked.connect(getattr(self, f"set_{menu.lower()}"))
+            self.right_menu_layout.addWidget(button)
     
     def set_main(self) :
         label1 = QLabel("main")
@@ -38,6 +53,7 @@ class CulturApp(QWidget) :
         
     def define_layout(self) :
         self.stat_layout.addLayout(self.main_layout)
-        self.right_menu_layout.addLayout(self.stat_layout)
+        self.window_layout.addLayout(self.right_menu_layout)
+        self.window_layout.addLayout(self.stat_layout)
         
-        self.layout.addLayout(self.right_menu_layout)
+        self.layout.addLayout(self.window_layout)
