c51ce63492954ac5c728828c88aed897fac2ce48
Florian-EL <florian.eymerleprovost@gmail.com>
2026-06-04 23:16:40 +0200

    création complete colonne pour film

--- src/models/film.py
+++ src/models/film.py
@@ -2,6 +2,15 @@ from dataclasses import dataclass
 
 @dataclass
 class Film:
-    id    : int = 0
-    titre : str = ""
-    note  : int = 0
+    id       : int = 0
+    titre    : str = ""
+    type     : str = ""
+    genre    : str = ""
+    vo       : str = ""
+    cinema   : bool = 0
+    updated  : str = ""
+    etat     : str = ""
+    annee_vu : int = 0
+    nb_vu    : int = 0
+    note     : int = 0
+
