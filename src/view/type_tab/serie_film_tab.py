39d4922059a103c8df6cbbbaeebd608cfef5f168
Florian-EL <florian.eymerleprovost@gmail.com>
2026-06-06 10:59:40 +0200

    serie film : création complete

--- src/view/type_tab/serie_film_tab.py
+++ src/view/type_tab/serie_film_tab.py
@@ -13,11 +13,12 @@ class SerieFilmWidget(QWidget):
         self.db = db
         self.data = self.db.get("serie_film", SerieFilm)
         
-        self.columns = ["Titre", "Note"]
+        self.columns = ["Nom_serie", "Film", "Titre", "Type", "Genre", "VO", "Cinema", 
+                        "Updated", "Etat", "Annee_vu", "Nb_vu", "Note"]
         
         layout = QVBoxLayout(self)
         self.table = QTableWidget()
-        self.table.setColumnCount(2)
+        self.table.setColumnCount(len(self.columns))
         self.table.setHorizontalHeaderLabels(self.columns)
         layout.addWidget(self.table)
         
@@ -51,11 +52,21 @@ class SerieFilmWidget(QWidget):
         self.table.setRowCount(0)  # Vide le tableau
         self.load()
     
-    def set_data(self, data) :
+    def set_data(self, data : SerieFilm) :
         row = self.table.rowCount()
         self.table.insertRow(row)
-        self.table.setItem(row, 0, QTableWidgetItem(data.titre))
-        self.table.setItem(row, 1, QTableWidgetItem(str(data.note)))
+        self.table.setItem(row, 0, QTableWidgetItem(data.nom_serie))
+        self.table.setItem(row, 1, QTableWidgetItem(data.film))
+        self.table.setItem(row, 2, QTableWidgetItem(data.titre))
+        self.table.setItem(row, 3, QTableWidgetItem(data.type))
+        self.table.setItem(row, 4, QTableWidgetItem(data.genre))
+        self.table.setItem(row, 5, QTableWidgetItem(data.vo))
+        self.table.setItem(row, 6, QTableWidgetItem(data.cinema))
+        self.table.setItem(row, 7, QTableWidgetItem(data.updated))
+        self.table.setItem(row, 8, QTableWidgetItem(data.etat))
+        self.table.setItem(row, 9, QTableWidgetItem(data.annee_vu))
+        self.table.setItem(row, 10, QTableWidgetItem(data.nb_vu))
+        self.table.setItem(row, 11, QTableWidgetItem(str(data.note)))
     
     def load(self):
         for data in self.data:
